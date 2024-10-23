bc6475177dba7da2c9f6a83480b1dd3478651b7d
YANN11GAB <danieloubassou@gmail.com>
2024-10-23 16:31:57 +0200

    file

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,12 +17,12 @@ lastdigit = n % 10;
 
 if (lastdigit > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n");
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 else if (lastdigit == 0)
 {
-printf("Last digit of %d is %d and is 0\n");
+printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
-printf("Last digit of %d is %d and is less than 6 and not 0\n");
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 return (0);
 }
