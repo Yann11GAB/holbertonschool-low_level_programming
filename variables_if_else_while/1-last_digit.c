21d42895a918c1f9b7f690e1f254308cc075dd3e
YANN11GAB <danieloubassou@gmail.com>
2024-10-23 16:40:15 +0200

    file

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -23,6 +23,9 @@ else if (lastdigit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, lastdigit);
 }
+else
+{
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
+}
 return (0);
 }
