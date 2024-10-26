b7fc898519e596a76da27f54e711d6a51f64642f
YANN11GAB <danieloubassou@gmail.com>
2024-10-26 18:45:43 +0200

    file

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+#include <stdio.h>
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+
+return (0);
+}
