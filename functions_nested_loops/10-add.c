6aca66b9819fd7dffed8ef1d0fb698fe3ff4e322
YANN11GAB <danieloubassou@gmail.com>
2024-10-27 19:02:31 +0100

    file

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,7 +2,9 @@
 /**
  * add - Write a function that adds two integers and returns the result.
  * Return: The result
- */ 
+ * @a: first digit
+ * @b: second digit
+ */
 int add(int a, int b)
 {
 return (a + b);
