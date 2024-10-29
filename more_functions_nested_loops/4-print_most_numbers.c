f3d7b545f48d1d62e57401f1bbc61d9c2b7168ad
YANN11GAB <danieloubassou@gmail.com>
2024-10-29 14:49:15 +0100

    file

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that prints the numbers
+ * Return: 0
+ */
+{
+void print_most_numbers(void);
+{
+for (i = 0 ; i <= 9; i++)
+}
+{
+if ( i != 2 ; i != 4)
+_putchar(i + 0)
+}
+_putchar('\n');
+}
