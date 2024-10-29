2163804bbc203c1e7b2b7f32b47a90ac86b28e5a
YANN11GAB <danieloubassou@gmail.com>
2024-10-29 19:12:59 +0100

    file

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * more_numbers -  a function that prints 10 times the numbers
+ * more_numbers - Write a function that prints 10 times the numbers 0 to 14.
+ * Description: Write a function that prints digits
  */
+
 void more_numbers(void)
 {
 int c;
 int i;
-
 for (i = 0; i <= 10; i++)
 {
 for (c = 0; c <= 14; c++)
