54b29854cdf0ca6c0a785960f2604b81c3f02443
YANN11GAB <danieloubassou@gmail.com>
2024-10-29 18:54:49 +0100

    file

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,15 +11,12 @@ for (i = 0; i <= 10; i++)
 {
 for (c = 0; c <= 14; c++)
 {
-if (c > 9)
-{
-_putchar('1');
-_putchar(c % 10 + '0');
+if (c > 10)
+{	
+_putchar((c / 10) + '0');
 }
-else
-{
+_putchar((c % 10) + '0');
+}
 _putchar('\n');
-}
-}
 }
 }
