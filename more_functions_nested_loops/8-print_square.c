8e2f8645584387a53da1d19fa349c4e19508b4f8
YANN11GAB <danieloubassou@gmail.com>
2024-10-30 12:43:53 +0100

    file

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_square - print block
+ * @size: is parameter
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+int a, b;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (a = 0; a < size; a++)
+{
+for (b = 0; b < size; b++)
+_putchar('#');
+}
+_putchar('\n');
+}
+}
