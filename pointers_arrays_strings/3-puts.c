7fe308771fedec0af9f7f0cfed02c88915b6b544
YANN11GAB <danieloubassou@gmail.com>
2024-11-02 18:01:09 +0100

    file

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Write a function that prints a string, followed by a new line.
+ * @str: string
+ */
+void _puts(char *str)
+{
+while (*str != '\n')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
