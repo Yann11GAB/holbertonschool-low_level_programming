c5445526bd2824ed28e17e545f7029d0a19957de
YANN11GAB <danieloubassou@gmail.com>
2024-11-03 14:43:05 +0100

    file

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - Write a function that prints half of a string.
+ * @str: is a string
+ */
+void puts_half(char *str)
+{
+int length = 0;
+int i;
+int j;
+
+while (str[length] != '\0')
+{
+length++;
+}
+if (length % 2 == 0)
+{
+i = length / 2;
+}
+else
+{
+i = (length / 2) + 1;
+}
+for (j = i; j < length; j++)
+{
+_putchar(str[j]);
+}
+_putchar('\n');
+}
