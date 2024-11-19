88fb686862c385a4422e07c95e7ee1cbdabee73b
YANN11GAB <danieloubassou@gmail.com>
2024-11-19 13:08:56 +0100

    file

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * print_name - write a function that prints a name.
  * @name: a pointer to a string
@@ -8,8 +8,9 @@
 void print_name(char *name, void (*f)(char *))
 {
 if (name && f)
-{
+
 f(name);
-}
+
 return;
+
 }
