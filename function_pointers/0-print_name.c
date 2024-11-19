61ba7ce9d282466728303e3fbaacff312cdc58ab
YANN11GAB <danieloubassou@gmail.com>
2024-11-19 12:12:37 +0100

    file

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - write a function that prints a name.
+ * @name: a pointer to a string
+ * @f: A pointer to a function that takes a char * 
+ * Return: return void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name && f)
+{
+f(name);
+}
+}
---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef MYFUNCTION_POINTERS_H
+#define MYFUCTION_POINTERS_H
+
+#include <stddef.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
