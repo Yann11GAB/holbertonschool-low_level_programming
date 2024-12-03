f504b20fc27ebb34e9e3de4476ad292c52375c77
YANN11GAB <danieloubassou@gmail.com>
2024-12-03 22:52:21 +0100

    file

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+
+#include "lists.h"
+
+/**
+ * print_list - Prints all the elements of a list_t list.
+ * @h: Pointer to the first node.
+ * Return: The number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+    size_t count = 0;
+
+    while (h)
+    {
+        if (h->str == NULL)
+            printf("[%lu] (nil)\n", count);
+        else
+            printf("[%lu] %s\n", count, h->str);
+        h = h->next;
+        count++;
+    }
+
+    return count;
+}
